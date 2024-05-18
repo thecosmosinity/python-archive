00af1c33d9f2152056cc78b494be2ffbcd5abcd4
thecosmosinity <160763775+thecosmosinity@users.noreply.github.com>
2024-05-18 15:49:11 +0530

    added a spam bot
    it works so epic!

---
+++ yapper/spam-bot.py
@@ -0,0 +1,5 @@
+from time import sleep as aihg
+
+while True:
+    aihg(0.0001)
+    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
